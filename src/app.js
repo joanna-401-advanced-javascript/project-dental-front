dcd15b72bb241cbe90348eb108fa8ee59ab3ef53
Joanna Arroyo <45890484+joannaarroyo@users.noreply.github.com>
2019-09-28 17:52:54 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// Import components here
+
+// Import stylesheets here
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Template works!</h1>
+
+        {/* Add components here */}
+
+      </>
+    );
+  }
+}
+
+export default App;
