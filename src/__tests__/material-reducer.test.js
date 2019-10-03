d4b247267deb343ea300f253809d4486daca4006
joannaarroyo <joliuac@gmail.com>
2019-10-03 00:08:12 -0700

    updated material reducer tests

--- src/__tests__/material-reducer.test.js
+++ src/__tests__/material-reducer.test.js
@@ -5,11 +5,27 @@ describe('Material reducer', () => {
     expect(materialReducer(undefined, [])).toEqual([]);
   });
 
+  test('should fetch all material', () => {
+    const fetchAction = {
+      type: 'MATERIAL_FETCH',
+      payload: [{ name: 'test' }],
+    };
+    expect(materialReducer([], fetchAction)).toEqual([{ name: 'test' }]);
+  });
+
   test('should create new material', () => {
     const createAction = {
-      type: 'MATERIAL_CREATE',
+      type: 'MATERIAL_ADD',
       payload: { name: 'test' },
     };
-    expect(materialReducer([], createAction)).toEqual([{ name: 'test' }]);
+    expect(materialReducer([{ name: 'original' }], createAction)).toEqual([{ name: 'original' }, { name: 'test' }]);
+  });
+
+  test('should update a material', () => {
+    const updateAction = {
+      type: 'MATERIAL_UPDATE',
+      payload: { name: 'update' , _id: 1},
+    };
+    expect(materialReducer([{ name: 'test' , _id: 1}], updateAction)).toEqual( [{ name: 'update' , _id: 1}] );
   });
 });
